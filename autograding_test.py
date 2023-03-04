1ef892409de05b34f623360bc6ce5db53e53417b
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-03-04 13:30:05 -0500

    Set Answer

--- autograding_test.py
+++ autograding_test.py
@@ -1,4 +1,4 @@
 import program;
 
 def test_program():
-    assert program.myFunction() == "Hello World!"
+    assert program.result == "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\n"
