06884af4287c785db6a0c18fad996cb59bfca707
Chris Atkinson <46910324+chrisatk@users.noreply.github.com>
2023-03-04 13:47:08 -0500

    Updated solution

--- autograding_test.py
+++ autograding_test.py
@@ -1,4 +1,37 @@
 import program;
 
+solution = '1\n'\
+'2\n'\
+'Fizz\n'\
+'4\n'\
+'Buzz\n'\
+'Fizz\n'\
+'7\n'\
+'8\n'\
+'Fizz\n'\
+'Buzz\n'\
+'11\n'\
+'Fizz\n'\
+'13\n'\
+'14\n'\
+'FizzBuzz\n'\
+'16\n'\
+'17\n'\
+'Fizz\n'\
+'19\n'\
+'Buzz\n'\
+'Fizz\n'\
+'22\n'\
+'23\n'\
+'Fizz\n'\
+'Buzz\n'\
+'26\n'\
+'Fizz\n'\
+'28\n'\
+'29\n'\
+'FizzBuzz\n'\
+'31\n'\
+'32\n'
+
 def test_program():
-    assert program.result == "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\n"
+    assert program.result == solution
